784bbd2670ae2eb0166de1f3e011b846002012da
wafaa122 <wafa_7_@hotmail.com>
2018-02-21 13:09:41 +0200

    6.14: ActiveJob and Advanced ActionCable

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -7,9 +7,22 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
+    received: function() {
+        // Called when there's incoming data on the websocket for this channel
+        $(".alert.alert-info").show();
+        $('.product-reviews').prepend(data.comment);
+        $("#average-rating").attr('data-score', data.average_rating);
+        refreshRating();
+    },
 
-      $(".alert.alert-info").show();
-  }
+    listen_to_comments: function() {
+
+        return this.perform('listen', {
+
+        product_id: $("[data-product-id]").data("product-id")
+        });
+    }
+});
+$(document).on('turbolinks:load', function() {
+    App.product.listen_to_comments();
 });
